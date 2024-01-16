b23f0f36ac89d154f7b7f9a5d8846166765716ec
ParallaxTelos <arthurdrummer@hotmail.com>
2024-01-16 19:51:05 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+// tipo de dados
+// booleanos
+
+// conversao implícita
+const numero = 456;
+const numeroString = Number("456");
+
+// console.log(numero === numeroString);
+
+//Number()
+//String()
+console.log(numero + numeroString);
+ 
+// conversao explícita
