aaa9d6c12d77cd1744ba82069f50151bf737cdc2
ParallaxTelos <arthurdrummer@hotmail.com>
2024-01-16 16:29:21 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,17 @@
+// tipo String
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const stringComNumeros = "34567";
+const citacao = "Meu nome é ";
+const meuNome = "Arthur";
+
+const senha1 = "minhaSenha123"
+console.log(senha1.length) // 13 caracteres
+
+//concatenação (+)
+
+console.log(citacao + meuNome);
+
+//tipo Template String ou Template Literal
+
